3459009c81fb13a429e7aeed1e5bc86cac6354bc
Kusal Kalinga <kusalkalingainfo@gmail.com>
2023-09-24 11:39:58 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const ws = require("ws");
+const server = new ws.Server({ port: "3000" });
+
+server.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    const b = Buffer.from(message);
+    console.log(b.toString());
+    console.log(message);
+    socket.send(`${message}`);
+  });
+});
