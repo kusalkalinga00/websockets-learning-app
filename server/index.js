b57acc3f5cb370daba0510b54fa433748228a13b
Kusal Kalinga <kusalkalingainfo@gmail.com>
2023-09-30 14:46:01 +0530

    express server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
-import { createServer } from "http";
+import express from "express";
 import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const httpServer = createServer();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const io = new Server(httpServer, {
+const port = process.env.PORT || 3500;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+const io = new Server(expressServer, {
   cors: {
     origin:
       process.env.NODE_ENV === "production"
@@ -19,8 +32,4 @@ io.on("connection", (socket) => {
     console.log(data);
     io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
   });
-});
-
-httpServer.listen(3500, () => {
-  console.log("Listening on port 3500");
 });
